b9cc6175ea17ed5f6bc81c855e518ed2d6602d4b
Admin <Admin@DESKTOP-C49GG1B>
2023-06-06 19:42:32 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp13/Calculator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Calculator
+    {
+        
+        public double A { get; set; }
+        public double B { get; set; }
+
+        public Calculator(double a, double b)
+        {
+            A = a;
+            B = b;
+        }
+        public override string? ToString()
+        {
+            if (B > 0)
+                return A + "+" + B;
+            else if (B < 0)
+                return A + "-" + Math.Abs(B);
+            return A.ToString();
+        }
+        public static Calculator operator +(Calculator c1, Calculator c2)
+        {
+            return new Calculator(c1.A + c2.A, c2.B + c2.B);
+        }
+        public static Calculator operator -(Calculator c1, Calculator c2)
+        {
+            return new Calculator(c1.A - c2.A, c2.B - c2.B);
+        }
+        public static Calculator operator *(Calculator c1, Calculator c2)
+        {
+            return new Calculator(c1.A * c2.A - c1.B * c2.B,
+                c1.A * c2.B - c2.A * c1.B);
+        }
+        public static Calculator operator /(Calculator c1, Calculator c2)
+        {
+            return new Calculator((c1.A * c2.A - c1.B * c2.B) / (c2.B * c2.B + c2.A * c2.A),
+                            (c1.A * c2.B - c2.A * c1.B) / (c2.B * c2.B + c2.A * c2.A));
+        }
+        public static bool operator ==(Calculator c1, Calculator c2)
+        {
+            return (c1.A == c2.A) && (c1.B == c2.B);
+        }
+        public static bool operator !=(Calculator c1, Calculator c2)
+        {
+            return (c1.A != c2.A) || (c1.B != c2.B);
+        }
+    }
+}
---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,15 @@
+﻿using ConsoleApp13;
+using System.Numerics;
+
+Calculator calculator1 = new Calculator(4, 7);
+Calculator calculator2 = new Calculator(5, 8);
+Calculator calculator3 = calculator1 + calculator2;
+Console.WriteLine(calculator3.ToString());
+Calculator calculator4 = calculator1 - calculator2;
+Console.WriteLine(calculator4.ToString());
+Calculator calculator5 = calculator1 * calculator2;
+Console.WriteLine(calculator5.ToString());
+Calculator calculator6 = calculator1 / calculator2;
+Console.WriteLine(calculator6.ToString());
+Console.WriteLine(calculator1 == calculator2);
+Console.WriteLine(calculator1 != calculator2);
